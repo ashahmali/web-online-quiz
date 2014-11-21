66b3ac6efcb0cec8313d6194fb3e926a09af8838
eduardo <ppjehg@gmail.com>
2014-11-21 14:18:04 +0000

    Header & Footer
    Header & Footer

---
+++ application/views/templates/header.php
@@ -0,0 +1,47 @@
+
+<html class="no-js" lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!-- Modernizer -->
+		<script src="<?php echo base_url('assets');?>/js/modernizr.js"></script>
+
+		<!--[if IE 8]>
+        	<script>var IE8 = true;</script>
+        	<script src="http://formstone.it/js/site.ie8.js"></script>
+			<link rel="stylesheet" href="http://formstone.it/css/demo.ie8.css">
+		<![endif]-->
+		<!--[if IE 9]>
+        	<script>var IE9 = true;</script>
+        	<script src="http://formstone.it/js/site.ie9.js"></script>
+		<![endif]-->
+
+		<link href="<?php echo base_url();?>assets/css/jquery.fs.shifter.css" rel="stylesheet" type="text/css" media="all">
+		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style_dropdown.css">
+		<script src="<?php echo base_url();?>assets/js/jquery-2.1.1.min.js" type="text/javascript"></script>
+		<script src="<?php echo base_url();?>assets/js/jquery.fs.shifter.js"></script>
+
+		<style>
+			/*.shifter .shifter-handle { float: right; margin: -5px 0 0; }*/
+			.shifter .shifter-navigation { background: #f9f9f9; padding: 100px 30px 30px; }
+			.shifter .shifter-navigation a { color: #666; display: block; font-size: 18px; margin: 0 0 15px; }
+		</style>
+
+		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" />
+		
+		<script>
+			$(document).ready(function() {
+				$.shifter({
+					maxWidth: Infinity
+				});
+			});
+		</script>
+		<!--[if lt IE 9]>
+		  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<title><?php echo $title ?> - Quiz System</title>
+		
+	</head>
+	<body class="gridlock demo shifter">
+	
