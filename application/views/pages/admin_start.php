350b9907542bf541691ab6109dc4eb63cac9e158
eduardo <ppjehg@gmail.com>
2014-11-27 14:39:18 +0000

    Fix - Change css class for admin pages

--- application/views/pages/admin_start.php
+++ application/views/pages/admin_start.php
@@ -8,7 +8,7 @@
  * @version     1.0.0
  */
 ?>
-<article class="row page register">
+<article class="row page admin">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		<header class="header">
 			<h1>Subjects Management</h1>
