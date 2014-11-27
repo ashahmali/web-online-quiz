c023d876850ce78410753be52ee6d81b75af69c6
eduardo <ppjehg@gmail.com>
2014-11-27 13:56:35 +0000

    Add - Admin template

---
+++ application/views/pages/admin_start.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Admin - Header Template
+ *
+ * @param       
+ * @author 		Ashiru Ali & Eduardo Hernandez
+ * @package 	Web Development/Cousework
+ * @version     1.0.0
+ */
+?>
+<article class="row page register">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<header class="header">
+			<h1>Subjects Management</h1>
+		</header>
