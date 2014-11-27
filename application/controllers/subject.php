040028e6971e0a7bbe94ac9af1a8a1e8821c8b35
eduardo <ppjehg@gmail.com>
2014-11-27 13:56:19 +0000

    Add - Subject Controller

---
+++ application/controllers/subject.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Subject extends CI_Controller {
+
+	
+	public function index()
+	{
+		$data['title'] = ucfirst('Subject Management'); // Capitalize the first letter
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('templates/header', $data);
+			$this->load->view('pages/admin_start', $data);
+			$this->load->view('pages/admin_subject_new', $data);
+			$this->load->view('pages/admin_subjects', $data);
+			$this->load->view('pages/admin_finish', $data);
+			$this->load->view('templates/footer', $data);
+		}
+		else
+		{
+			$this->load->view('templates/header', $data);
+			//validate credentials
+
+			//create and update user session
+
+			//validate user role and load the proper profile view
+			
+
+			$this->load->view('templates/footer', $data);
+			
+		}
+
+	}
+}
