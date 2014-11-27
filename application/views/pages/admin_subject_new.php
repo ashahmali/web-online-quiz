15ccaa58f65a41ed069d7abd62052c01ad35cbfc
eduardo <ppjehg@gmail.com>
2014-11-27 13:57:18 +0000

    Add - New subject form

---
+++ application/views/pages/admin_subject_new.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Admin - Create a Subject Template
+ *
+ * @param       
+ * @author 		Ashiru Ali & Eduardo Hernandez
+ * @package 	Web Development/Cousework
+ * @version     1.0.0
+ */
+?>
+
+<form class="subject_new">
+	<div class="row" style="margin-bottom:10px;">
+		<div class="col-1 col-lg-offset-1 col-lg-3 col-md-3 col-sm-3 col-xs-12">
+			<label for="first_name">Subject</label>
+		</div>
+		<div class="col-2 col-lg-3 col-md-4 col-sm-5 col-xs-12">
+			<input type="text" name="first_name" placeholder="Subject Name"/>
+		</div>
+		<div class="col-2 col-lg-3 col-md-3 col-sm-4 col-xs-12">
+			<button>Create Subject</button>
+		</div>
+	</div>
+</form>
+
+		
