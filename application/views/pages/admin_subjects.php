9eec4d69c40e120459f99674f5f5bac0649a0a3f
eduardo <ppjehg@gmail.com>
2014-11-27 13:57:03 +0000

    Add- Subjects lists template

---
+++ application/views/pages/admin_subjects.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * List Subjects
+ *
+ * @author 		Ashiru Ali & Eduardo Hernandez
+ * @package 	Web Development/Cousework
+ * @version     1.0.0
+ */
+?>
+
+<div class="subjects_list row">
+	<div class="col-lg-4 col-md-4 col-sm-3 col-xs-2">
+	</div>
+	<div class="col-lg-4 col-md-4 col-sm-6 col-xs-8">
+		<!-- list starts -->
+		<div class="row subject">
+			<div class=" col-sm-8 col-xs-9">
+				<p>Web Development</p>
+			</div>
+			<div class="col-sm-4 col-xs-3">
+				<a href="#" alt="edit subject" title="edit subject"><i class="fa fa-pencil-square-o"></i></a>
+			</div>
+		</div>
+		<div class="row subject">
+			<div class=" col-sm-8 col-xs-9">
+				<p>Web Development</p>
+			</div>
+			<div class="col-sm-4 col-xs-3">
+				<a><i class="fa fa-pencil-square-o"></i></a>
+			</div>
+		</div>
+		<div class="row subject">
+			<div class=" col-sm-8 col-xs-9">
+				<p>Web Development</p>
+			</div>
+			<div class="col-sm-4 col-xs-3">
+				<a><i class="fa fa-pencil-square-o"></i></a>
+			</div>
+		</div>
+		<!-- list finishes-->
+
+		<!-- pagination -->
+		<div class="row pagination">
+			<a class="col-xs-2 text-center">
+				<i class="fa fa-chevron-left"></i>
+			</a>
+			<div class="col-xs-1 text-center">
+				<p class="splitter">|</p>
+			</div>
+			<a class="col-xs-2 text-center">
+				<i class="fa fa-chevron-right"></i>
+			</a>
+		</div>
+		<!-- <div class="row pagination">
+			<div class="col-xs-2 text-center">
+				<a><i class="fa fa-chevron-left"></i></a>
+			</div>
+			<div class="col-xs-1 text-center">
+				<p class="splitter">|</p>
+			</div>
+			<div class="col-xs-2 text-center">
+				<a><i class="fa fa-chevron-right"></i></a>
+			</div>
+		</div> -->
+		<!-- end pagination -->
+	</div>
+	<div class="col-lg-4 col-md-4 col-sm-3 col-xs-2">
+	</div>
+</div>
