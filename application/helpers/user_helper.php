4fea4d4362899cc74675fea4aeb5d0f681e1f2fc
ashahmali <ashahmali@Ashirus-MacBook-Pro.local>
2014-11-30 03:03:27 +0000

    refactoring

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -4,5 +4,5 @@ function log_user_in($user_data){
 	//print_r($user_data);
 	print_r($this->session->all_userdata());
 	$this->session->set_userdata("usr", "me");
-	//redirect('users/def_page');
+	redirect('users/def_page');
 }
