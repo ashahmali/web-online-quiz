4770ce10e5a646c82e215dbf676a6a30ffa2b645
eduardo <ppjehg@gmail.com>
2014-12-02 10:48:18 +0000

    Feature - Use Quiz dummy data

--- application/controllers/quiz.php
+++ application/controllers/quiz.php
@@ -7,6 +7,11 @@ class Quiz extends CI_Controller {
 	{
 		$data['heading'] = '';
 		$data['showQuizNav'] = true;
+		$data['questions'] = array(
+				'Question 1' => array('Answer 1 Question 1','Answer 2 Question 1','Answer 3 Question 1'),
+				'Question 2' => array('Answer 1 Question 2','Answer 2 Question 2','Answer 3 Question 2'),
+				'Question 3' => array('Answer 1 Question 3','Answer 2 Question 3','Answer 3 Question 3')
+			);
 		$this->load->view('templates/header', $data);
 		$this->load->view('pages/testtaker_start', $data);
 		$this->load->view('pages/testtaker_quiz', $data);
