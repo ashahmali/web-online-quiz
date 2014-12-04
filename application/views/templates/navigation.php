5af99c8b22b3473e5bfea6b5a9a736c67d0ccf3f
eduardo <ppjehg@gmail.com>
2014-12-04 11:25:09 +0000

    Feature - New navigation sytyle

--- application/views/templates/navigation.php
+++ application/views/templates/navigation.php
@@ -1,7 +1,34 @@
+<?php
+/**
+ * NAvigation Template
+ *
+ * @author    Ashiru Ali & Eduardo Hernandez
+ * @package   Web Development/Cousework
+ * @version   1.0.0
+ */
+?>
+
 <nav class="shifter-navigation">
-	<a href="#1">1</a>
+	<!-- <a href="#1">1</a>
 	<a href="#2">2</a>
 	<a href="#3">3</a>
 	<a href="#4">4</a>
-	<a href="#5">5</a>
+	<a href="#5">5</a> -->
+	<ul class="menu_options">
+		<li><a>Log Out</a></li>
+		<li><a>Home</a></li>
+		
+	<?php 
+		if ($this->session->userdata('ROLE_idROLE') == 1){
+			?>
+				<li>Manage Users</li>
+				<li>Create Subject</li>
+				<li>Create Test</li>
+				<li>Add Questions</li>
+			<?php
+		}
+	?>
+
+	</ul>
+
 </nav>
