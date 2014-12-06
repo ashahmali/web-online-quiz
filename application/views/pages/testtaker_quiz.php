72676988594ef4a80a0981e1d85093a65b6bbabb
eduardo <ppjehg@gmail.com>
2014-12-06 13:34:22 +0000

    Fix - Add a form control to send test data

--- application/views/pages/testtaker_quiz.php
+++ application/views/pages/testtaker_quiz.php
@@ -9,7 +9,7 @@
 ?>
 
 
-<div class="testtaker_quiz row ">
+<div class="testtaker_quiz row">
 
 	<div class="col-1 col-sm-6 col-xs-12 text-right subject_container">
 		<p>Subject:</p>
@@ -24,38 +24,40 @@
 		<p class="name">Web Performance and Optimization</p>
 	</div>
 
-	<div class="col-xs-12 text-center">
-		<p>General Intructions</p>
-	</div>
-	<?php $counterQuestion = 1; ?>
-	<?php foreach ($questions as $q) { ?>
-		<!-- question start-->
-		<div id="question_<?php echo $counterQuestion; ?>" class="col-xs-12 question">
-			<div class="row statement">
-				<div class="col-sm-12">
-					<p><span><?php echo $counterQuestion; ?> - </span><?php echo $q['statement'];?></p> 
+	<form id="#test_form" >
+		<div class="col-xs-12 text-center">
+			<p>General Intructions</p>
+		</div>
+		<?php $counterQuestion = 1; ?>
+		<?php foreach ($questions as $q) { ?>
+			<!-- question start-->
+			<div id="question_<?php echo $counterQuestion; ?>" class="col-xs-12 question">
+				<div class="row statement">
+					<div class="col-sm-12">
+						<p><span><?php echo $counterQuestion; ?> - </span><?php echo $q['statement'];?></p> 
+					</div>
+				</div>
+				<div class="row answers">
+					<?php $counterAnswers = 1; ?>
+					<?php foreach ($q['options'] as $option) { ?>
+						<div class=" col-sm-12 answer">
+							<div class="radio">
+								<input id="answer_<?php echo $counterQuestion.'_'.$counterAnswers; ?>" type="radio" name="answers_<?php echo $counterQuestion; ?>" value="<?php echo $q['id'].'_'.$option['id'] ;?>" style="display:none;">  
+							    <label for="answer_<?php echo $counterQuestion.'_'.$counterAnswers; ?>" class="radio_label"><?php echo $option['description'];?></label> 
+							</div>
+						</div>
+						<?php $counterAnswers++; ?>
+					<?php } ?>
 				</div>
 			</div>
-			<div class="row answers">
-				<?php $counterAnswers = 1; ?>
-				<?php foreach ($q['options'] as $option) { ?>
-					<div class=" col-sm-12 answer">
-						<div class="radio">
-							<input id="answer_<?php echo $counterQuestion.'_'.$counterAnswers; ?>" type="radio" name="answers_<?php echo $counterQuestion; ?>" value="<?php echo $q['id'].'_'.$option['id'] ;?>" style="display:none;">  
-						    <label for="answer_<?php echo $counterQuestion.'_'.$counterAnswers; ?>" class="radio_label"><?php echo $option['description'];?></label> 
-						</div>
-					</div>
-					<?php $counterAnswers++; ?>
-				<?php } ?>
-			</div>
-		</div>
-		<!-- question finish-->
-	<?php $counterQuestion++; ?>
-	<?php } ?>
-	
+			<!-- question finish-->
+		<?php $counterQuestion++; ?>
+		<?php } ?>
+		
 
-	<div class="col-xs-12 text-center">
-		<button>Submit</button>
-	</div>
+		<div class="col-xs-12 text-center">
+			<button>Submit</button>
+		</div>
+	</form>
 
 </div>
