a09afaa92ff6eb0c46add326399b0cdb7fa32d06
eduardo <ppjehg@gmail.com>
2014-12-06 18:19:42 +0000

    Add - Quiz Error

---
+++ application/views/pages/testtaker_quiz_error.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Test taker - Quiz Error
+ *
+ * @author 		Ashiru Ali & Eduardo Hernandez
+ * @package 	Web Development/Cousework
+ * @version     1.0.0
+ */
+?>
+
+
+<div class="testtaker_quiz_error row ">
+
+	<div class="col-xs-12 text-center subject_container">
+		<p>We could not provide the information for the requested quiz. Contact the sys admin.</p>
+	</div>
+	
+	<div class="col-xs-12 text-center">
+		<button>Go Back Home</button>
+	</div>
+
+</div>
