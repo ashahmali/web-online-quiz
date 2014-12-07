7abf13a1ebd408a5a7fe46e6b1fd7400a62da4b7
eduardo <ppjehg@gmail.com>
2014-12-07 17:08:13 +0000

    FIX - NAVIGATION LINKS

--- application/views/templates/navigation.php
+++ application/views/templates/navigation.php
@@ -15,16 +15,16 @@
 	<a href="#4">4</a>
 	<a href="#5">5</a> -->
 	<ul class="menu_options">
-		<li><a>Log Out</a></li>
-		<li><a>Home</a></li>
+		<li><a href="users/logout" alt="Manage Users" title="Manage Users">Log Out</a></li>
+		<li><a href="home" alt="Manage Users" title="Manage Users">Home</a></li>
 		
 	<?php 
-		if ($this->session->userdata('ROLE_idROLE') == 1){
+		if (isset($showAdminMenu) && $showAdminMenu){
 			?>
-				<li>Manage Users</li>
-				<li>Create Subject</li>
-				<li>Create Test</li>
-				<li>Add Questions</li>
+				<li><a href="admin/manage_users" alt="Manage Users" title="Manage Users">Manage Users</a></li>
+				<li><a href="admin/add_subject" alt="Create Subject" title="Create Subject">Create Subject</a></li>
+				<li><a href="admin/quizzes" alt="Create Test" title="Create Test">Create Test</a></li>
+				<li><a href="admin/questions" alt="Add Questions" title="Add Questions">Add Questions</a></li>
 			<?php
 		}
 	?>
