eb151b5d2df90e409de653312eb8278a6553a0a9
eduardo <ppjehg@gmail.com>
2014-12-07 17:12:42 +0000

    FEature - Get correct answer

--- application/models/option_model.php
+++ application/models/option_model.php
@@ -18,6 +18,24 @@ class Option_model extends CI_Model {
     	}
         return $data;
 	}
+
+	public function getCorrectAnswer($question_id){
+		$data = array();
+		log_message('question_id', $question_id);
+		if(isset($question_id) && $question_id != ""){
+			$this->db->select('OPTION.idOPTION');
+			$this->db->from('OPTION');
+			$array = array('QUESTION_idQUESTION' => $question_id,'bCorrectAnswer' => 1);
+			$this->db->where($array); 
+			$query = $this->db->get();
+			if ($query->num_rows() > 0) {
+	            foreach ($query->result_array() as $row) {
+	                $data[] = $row;
+	            }
+	        }
+    	}
+        return $data;
+	}
 	
 }
 ?>
